a0e48d21649d2946cdbb9d7c2eac0d6cb8d7280b
marcelo-schreiber <maraugrissch@gmail.com>
2023-05-01 18:11:33 -0300

    Refac: check if it's bomb in obstacle

--- toolbox.py
+++ toolbox.py
@@ -1,11 +1,10 @@
 def check_group_positions(pos, group):
     """Checks if a position is occupied by a member of a group."""
     for member in group:
-        try:
-            if member.is_bomb:
-                continue
-        except AttributeError:
-            pass
+        if hasattr(
+            member, "is_bomb"
+        ):  # if is bomb, it's a obstacle sprite, but explodable
+            continue
 
         if member.rect.topleft == pos:
             return True

9f019fe1cfae5380f6180a89ea6209a50a6ad51b
marcelo-schreiber <maraugrissch@gmail.com>
2023-05-26 20:20:36 -0300

    Fix: explosion sprite

--- explosion.py
+++ explosion.py
@@ -1,36 +1,10 @@
-import pygame
 from tile import Tile
-from settings import EXPLOSION_TIME, TILE_SIZE, TILES_PATH
+from settings import EXPLOSION_TIME
 
 
 class Explosion(Tile):
     def __init__(self, pos, groups):
-        super().__init__(pos, groups, "explosion_center.png")
-
-        self.image_horizontal = pygame.image.load(f"{TILES_PATH}/explosion_path.png")
-        self.image_horizontal = pygame.transform.scale(
-            self.image_horizontal, (TILE_SIZE, TILE_SIZE)
-        )
-
-        self.image_corner = pygame.image.load(f"{TILES_PATH}/explosion_edge.png")
-        self.image_corner = pygame.transform.scale(
-            self.image_corner, (TILE_SIZE, TILE_SIZE)
-        )
-
-        self.image_center = pygame.image.load(f"{TILES_PATH}/explosion_center.png")
-        self.image_center = pygame.transform.scale(
-            self.image_center, (TILE_SIZE, TILE_SIZE)
-        )
-
-        self.images_list = {
-            "center": self.image_center,
-            "horizontal": self.image_horizontal,
-            "vertical": pygame.transform.rotate(self.image_horizontal, 90),
-            "corner_left": self.image_corner,
-            "corner_right": pygame.transform.flip(self.image_corner, True, False),
-            "corner_down": pygame.transform.rotate(self.image_corner, 90),
-            "corner_up": pygame.transform.rotate(self.image_corner, 270),
-        }
+        super().__init__(pos, groups, "explosion.png")
 
         self.timer = EXPLOSION_TIME
 
